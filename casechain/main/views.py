c8b0341555b6ae92dae199ab059dff2fd591ab22
lmeisege <lukas.meisegeier@capgemini.com>
2017-02-08 17:47:01 +0100

    added import case method

--- casechain/main/views.py
+++ casechain/main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from . import models
+from . import hash
 import hashlib
+import json
+from django.http.response import HttpResponse
+
 
 # Create your views here.
 
@@ -26,6 +30,25 @@ class CaseViews:
         Adds a new case to the chain
         """
         pass
+
+    def receiveCase(self,request):
+        """
+        Method to receive a case from other nodes
+        """
+        if 'case' in request.POST:
+            caseJson=json.loads(request.POST['case'])
+            case=models.Case(date=caseJson['date'],
+                             court=caseJson['court'],
+                             plaintiff=caseJson['plaintiff'],
+                             defendant=caseJson['defendant'],
+                             hashValue=caseJson['hashValue'],
+                             prevHashValue=caseJson['prevHashValue'],
+                             nonce=caseJson['nonce']
+                             )
+            if string(hashLib.calculateHash(case)) != string(caseJson['hashValue']):
+                error="sha256 hash incorrect"
+                return HttpResponse(json.dumps({'error':error}))
+
 
     def __createHashValue(self,caseId):
         """
