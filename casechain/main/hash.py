8554f92ee468c52c2bbf4d73da3e36e87f3b4cad
jathomas <jakob.thomas@capgemini.com>
2017-02-08 19:51:27 +0100

    added has function without db

--- casechain/main/hash.py
+++ casechain/main/hash.py
@@ -20,11 +20,31 @@ def calculateHash (case_id):
 
     return hash.hexdigest()
 
+def calculateHashNoId (case, verdicts, statementOfFacts, facts, consensuses, views):
+    # create sha256 object
+    hash = hashlib.sha256()
+    # insert hash of data
+    hash.update(bytes(caseToStringNoId(case, verdicts, statementOfFacts, facts, consensuses, views), "utf-8"))
+
+    print(hash.hexdigest())
+
+    return hash.hexdigest()
+
 def caseToString (case):
 
     case_as_string = repr(case.date) + repr(case.court) + repr(case.plaintiff) + repr(case.defendant) + repr(case.nonce)\
                      + factsToString(case) + consensusesToString(case) + viewsToString(case) +\
-                     verdictToString(case)
+                     verdictsToString(case)
+
+    print(case_as_string)
+
+    return case_as_string
+
+def caseToStringNoId (case, verdicts, statementOfFacts, facts, consensuses, views):
+
+    case_as_string = repr(case.date) + repr(case.court) + repr(case.plaintiff) + repr(case.defendant) + repr(case.nonce)\
+                     + factsToStringNoId(facts) + consensusesToStringNoId(consensuses) + viewsToStringNoId(views) +\
+                     verdictsToStringNoId(verdicts)
 
     print(case_as_string)
 
@@ -39,9 +59,23 @@ def factsToString (case):
 
     return fact_string
 
+def factsToStringNoId (facts):
+    fact_string = ""
+    for fact in facts:
+        fact_string= fact_string + fact.fact +'\n'
+
+    return fact_string
+
 def consensusesToString (case):
     sof = StatementOfFacts.objects.get(case=case.id)
     consensuses = Consenus.objects.filter(statementOfFacts=sof.id)
+    consensus_string = ""
+    for consensus in consensuses:
+        consensus_string= consensus_string + consensus.opinion +'\n'
+
+    return consensus_string
+
+def consensusesToStringNoId (consensuses):
     consensus_string = ""
     for consensus in consensuses:
         consensus_string= consensus_string + consensus.opinion +'\n'
@@ -63,7 +97,20 @@ def viewsToString (case):
 
     return view_string
 
-def verdictToString (case):
+def viewsToStringNoId (views):
+    plaintiff_string = ""
+    defendant_string = ""
+    for view in views:
+        if view.viewer_types == "plaintiff":
+            plaintiff_string = plaintiff_string + view.view +'\n'
+        else:
+            defendant_string = defendant_string + view.view +'\n'
+
+    view_string = plaintiff_string + defendant_string
+
+    return view_string
+
+def verdictsToString (case):
     verdicts = Verdict.objects.filter(case=case.id)
     verdict_string = ""
     for verdict in verdicts:
@@ -72,5 +119,16 @@ def verdictToString (case):
         else:
             end = verdict.text
 
-    verdict_string = verdict_string +  end
+    verdict_string = verdict_string + end
+    return verdict_string
+
+def verdictsToStringNoId (verdicts):
+    verdict_string = ""
+    for verdict in verdicts:
+        if verdict.verdict_type == "intermediate":
+            verdict_string = verdict_string + verdict.text +'\n'
+        else:
+            end = verdict.text
+
+    verdict_string = verdict_string + end
     return verdict_string
